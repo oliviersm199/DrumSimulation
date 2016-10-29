61aec74420869e61d70013634bc8939af7a0c0f0
Xue Yang WANG <xwang184@ds2.CS.McGill.CA>
2016-10-29 16:58:08 -0400

    inner update

--- grid_4_4.c
+++ grid_4_4.c
@@ -8,6 +8,10 @@
 #define RHO 0.5
 #define G 0.75
 #define SIZE 4
+#define TAG1 1
+#define TAG2 2
+#define TAG3 3
+#define TAG4 4
 
 
 int main(int argc, char** argv) {
@@ -16,6 +20,7 @@ int main(int argc, char** argv) {
 
 
     //get the number of iterations
+    int *grid;
     char *ptr;
     long iterations;
     iterations = strtol(argv[1], &ptr, 10);
@@ -33,7 +38,12 @@ int main(int argc, char** argv) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-
+    //if (rank==0){
+     grid = malloc(16 *  sizeof(int));
+     for (int i=0;i<16;i++){
+     	grid[i]=0;
+     }
+    //}
     // each 16 processes will be mapped to a particular point. 
     //    0  1   2    3 
     //    4  5   6    7 
@@ -42,6 +52,10 @@ int main(int argc, char** argv) {
 
     int xPosition = rank % SIZE; // get the x value 
     int yPosition = rank / SIZE; // get the y value
+
+    MPI_Request request;
+    MPI_Status status;
+
 
     // set the default starting value of the drum to zero. 
     // otherwise set to 1 if at position SIZE/2
@@ -53,7 +67,10 @@ int main(int argc, char** argv) {
 
     double prevPrevValue = 0;
     double prevValue = 0;
-
+    double neighbourUp=0;
+    double neighbourDown=0;
+    double neighbourLeft=0;
+    double neighbourRight=0;
 
 
     //starting the iterations of the drum
@@ -66,6 +83,7 @@ int main(int argc, char** argv) {
 
         // top left corner
         if(xPosition == 0 && yPosition==0){
+
 
         }
         // top right corner
@@ -89,19 +107,40 @@ int main(int argc, char** argv) {
 
         }
         //left side 
-        else if(yPosition==0 && XPosition >=1 && XPosition <= SIZE-2){
+        else if(yPosition==0 && xPosition >=1 && xPosition <= SIZE-2){
 
         }
         //right side
-        else if(yPosition==SIZE-1 && XPosition >=1 && XPosition <= SIZE-2){
+        else if(yPosition==SIZE-1 && xPosition >=1 && xPosition <= SIZE-2){
 
         }
         //interior element
         else{
 
+        	MPI_Isend(&prevValue, 1, MPI_DOUBLE, (4*(yPosition-1))+xPosition, TAG1, MPI_COMM_WORLD, &request);
+        	MPI_Isend(&prevValue, 1, MPI_DOUBLE, (4*(yPosition+1))+xPosition , TAG2, MPI_COMM_WORLD, &request);
+        	MPI_Isend(&prevValue, 1, MPI_DOUBLE, (4*yPosition)+(xPosition-1), TAG3, MPI_COMM_WORLD, &request);
+        	MPI_Isend(&prevValue, 1, MPI_DOUBLE, (4*yPosition-1)+(xPosition+1), TAG4, MPI_COMM_WORLD, &request);
+    		MPI_Wait(&request, MPI_STATUS_IGNORE);
+    		MPI_Irecv(&neighbourUp,1,MPI_DOUBLE,(4*(yPosition-1))+xPosition,TAG1,MPI_COMM_WORLD,&request);
+    		MPI_Irecv(&neighbourDown,1,MPI_DOUBLE,(4*(yPosition+1))+xPosition, TAG2,MPI_COMM_WORLD,&request);
+    		MPI_Irecv(&neighbourLeft,1,MPI_DOUBLE,(4*yPosition)+(xPosition-1), TAG3,MPI_COMM_WORLD,&request);
+    		MPI_Irecv(&neighbourRight,1,MPI_DOUBLE,(4*yPosition-1)+(xPosition+1), TAG4,MPI_COMM_WORLD,&request);
         }
     }
 
+    if (rank==15){
+    int x =0;
+	    for (int i =0; i<4;i++){
+	    	for (int j =0; j <4;j++){
+	    	printf("%d--(%d,%d) ",grid[x],i,j);
+	    	x++;
+	    	//printf("%d",rank);	
+	    	}
+	    	printf("\n");
+	    }
+	}
+    
 
     // Finalize the MPI environment.
     MPI_Finalize();
